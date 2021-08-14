18ba8867add17e8626620f8674ab595f8db57e1e
glen-s-abraham <glenprojects666@gmail.com>
2021-08-14 13:08:56 +0530

    feat:Added CRUD to States endpoint

---
+++ app/app/Http/Controllers/StateController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\State;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+
+class StateController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return $this->showCollectionAsResponse(State::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        if($request->has('name'))
+        {
+            $state = State::create($request->only(['name']));
+            return $this->showModelAsResponse($state);
+        }
+        throw ValidationException::withMessages(['name'=>['required']]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\State  $state
+     * @return \Illuminate\Http\Response
+     */
+    public function show(State $state)
+    {
+        return $this->showModelAsResponse($state);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\State  $state
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, State $state)
+    {
+        if($request->has('name'))
+        {
+            $state->update($request->only(['name']));
+            return $this->showModelAsResponse($state);
+        }
+        throw ValidationException::withMessages(['name'=>['required']]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\State  $state
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(State $state)
+    {
+        $state->delete();
+        return $this->showModelAsResponse($state);
+    }
+}
--- app/routes/api.php
+++ app/routes/api.php
@@ -15,4 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::resource('countries','CountryController');
+Route::resource('states','StateController');
 Route::resource('educations','EducationController');
+
