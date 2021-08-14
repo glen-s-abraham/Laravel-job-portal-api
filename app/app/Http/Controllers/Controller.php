257632016f6cef86f5c5f06ba26c16d2bcb78916
glen-s-abraham <glenprojects666@gmail.com>
2021-08-14 12:17:29 +0530

    feat:CRUD fro country endpoint

--- app/app/Http/Controllers/Controller.php
+++ app/app/Http/Controllers/Controller.php
@@ -6,8 +6,31 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Collection;
+use Illuminate\Database\Eloquent\Model;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function successResponse($data,$code)
+	{
+		return response()->json($data,$code);
+	}
+
+	public function errorResponse($message,$code)
+	{
+		return response()->json(["error"=>$message,"code"=>$code]);
+	}
+
+	public function showCollectionAsResponse(Collection $collection,$code=200)
+	{
+		return $this->successResponse(["data"=>$collection],$code);
+	}	
+
+	public function showModelAsResponse(Model $model,$code=200)
+	{
+		
+		return $this->successResponse(["data"=>$model],$code);
+	}
 }
---
+++ app/app/Http/Controllers/CountryController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Country;
+use Illuminate\Http\Request;
+
+class CountryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return $this->showCollectionAsResponse(Country::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        if($request->has('name'))
+        {
+            $country = Country::create($request->only(['name']));
+            return $this->showModelAsResponse($country);
+        }
+        return $this->errorResponse('Bad Request',400);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Country  $country
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Country $country)
+    {
+        return $this->showModelAsResponse($country);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Country  $country
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Country $country)
+    {
+        if($request->has('name'))
+        {
+            $country->update($request->only(['name']));
+            return $this->showModelAsResponse($country);
+        }
+        return $this->errorResponse("Bad request",400);
+        
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Country  $country
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Country $country)
+    {
+        $country->delete();
+        return $this->showModelAsResponse($country);
+    }
+}
